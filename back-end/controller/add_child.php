e60d3ec2f045a54431708c50d67e4ab231188f06
Arvin Mai <Arvin Mai>
2016-11-28 22:57:51 -0800

    test

--- back-end/controller/add_child.php
+++ back-end/controller/add_child.php
@@ -17,9 +17,8 @@ $newChild = new ChildProfiles(NULL, NULL, NULL, NULL, NULL, NULL, NULL, $_POST['
         , $_POST['start_date'], $_POST['withdraw_date'], $_POST['withdraw_reason'], $_POST['first_name']
         , $_POST['last_name'], $_POST['chinese_name'], $_POST['nick_name'], $_POST['sex'], $_POST['age']
         , $_POST['birthday'], $_POST['primary_language'], $_POST['address'], $_POST['phone'], $_POST['child_status']);
-$newChildJson = $newChild->convertToJson();
 $newArray = $newChild->convertAsArray();
-echo json_encode($newArray);
-$newChildId = ChildProfilesRepository::addChild($newChildJson);
+$newChildId = ChildProfilesRepository::addChild(json_encode($newArray));
 $child = ChildProfilesRepository::getChildById($newChildId);
+echo $child;
 ?>
