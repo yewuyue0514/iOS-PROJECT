e08db472afc9ae819980e5e573a4c5df97b83dad
Arvin Mai <Arvin Mai>
2016-11-29 10:52:07 -0800

    http 500

--- back-end/testing/test.php
+++ back-end/testing/test.php
@@ -8,9 +8,13 @@
  * Date and Time: Nov 28, 2016 10:46:50 PM 
  * Project Name: CS556_Team_Project 
  */
-
+include_once '../model/child_profiles_repository.php';
+include_once '../model/child_profiles.php';
+include_once '../db/db_context.php';
+require '../db/db_connect.php';
+$db = DBContext::getDB();
 // Read request parameters
-$firstName = $_REQUEST["firstName"];
-$lastName = $_REQUEST["lastName"]; // Store values in an array
-$returnValue = array("firstName" => $firstName, "lastName" => $lastName); // Send back request in JSON format
-echo json_encode($returnValue);
+//$firstName = $_REQUEST["firstName"];
+//$lastName = $_REQUEST["lastName"]; // Store values in an array
+//$returnValue = array("firstName" => $firstName, "lastName" => $lastName); // Send back request in JSON format
+//echo json_encode($returnValue);
